4fc4f0d7bdf527d4a923224f46f39dcb3875bb5d
amonge20 <2023_aitor.monge@iticbcn.cat>
2024-05-01 13:31:49 +0200

    Practica 2 Django Crud Docker

--- Practica 2/DjangoCrud/CrudAPP/urls.py
+++ Practica 2/DjangoCrud/CrudAPP/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from CrudAPP import views
 
 urlpatterns = [
-    path('', views.teachers, name='teachers'),
+    path('', views.users, name='users'),
     path('create-form/', views.create, name='create'),
     path('update-form/<int:pk>', views.update, name='update'),
     path('delete-form/<int:pk>', views.delete, name='delete'),
